bfaa20d85c851ea803d1c7922f36ca07036ba8fe
Luiz Cestaro <99103680+Cestaro0@users.noreply.github.com>
2024-04-12 20:08:02 -0300

    Update script2.bat

--- 1º - Semestre/Sexta - Feira/TI nas Organizações/12-04-2024/script2.bat
+++ 1º - Semestre/Sexta - Feira/TI nas Organizações/12-04-2024/script2.bat
@@ -1,3 +1,5 @@
+@echo off
 rem usando defined agora para ver se a variavel está criada
-
-if defined nome (echo o nome e %nome%) else (echo variavel nao encontrada!!!)
+if defined (echo o nome e %nome%) else (echo variavel nao encontrada!!!)
+rem usando not defined, para verificar se está valida
+if not defined cidade (echo variavel nao definida!!!) else (echo a cidade e %cidade%)
